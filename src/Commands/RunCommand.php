81e364297a1cea09db9b4233a58cca03ee162257
H O S S E ! N <4.ho3ein@gmail.com>
2026-02-13 22:31:44 +0330

    feat: Execution command completion

--- src/Commands/RunCommand.php
+++ src/Commands/RunCommand.php
@@ -2,6 +2,9 @@
 
 namespace Iwh3n\Tgram\Commands;
 
+use Iwh3n\Tgram\ConfigManager;
+use Iwh3n\Tgram\UpdateHandler;
+use Iwh3n\Tgram\UpdateManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -18,6 +21,24 @@ class RunCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+        $cm = new ConfigManager();
+        $isConfig = $cm->isConfigFile();
+        $config = $isConfig ? $cm->getConfigFile() : false;
+
+        if (!$isConfig or !$config) {
+            $io->writeln("<fg=red>[ERROR]</> Configuration file not found.");
+            return Command::FAILURE;
+        }
+
+        if (isset($config['bot']) and isset($config['bot']['token']) and isset($config['bot']['entry_point'])) {
+            $uh = new UpdateHandler(new UpdateManager($config['bot']['token']));
+
+            $io->writeln("<fg=green>[OK]</> Running...");
+
+            $uh->handle($config['bot']['entry_point']);
+        }
+
         return Command::SUCCESS;
     }
 }
