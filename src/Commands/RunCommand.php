4ec755ed78d4cc21bf48d29e77ec16232099f697
H O S S E ! N <4.ho3ein@gmail.com>
2026-02-13 19:51:06 +0330

    refactor: Remove deleted dependencies

--- src/Commands/RunCommand.php
+++ src/Commands/RunCommand.php
@@ -7,9 +7,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-use Iwh3n\Tgram\Config\ConfigManager;
-use Iwh3n\Tgram\UpdateManager\UpdateManager;
-
 class RunCommand extends Command
 {
     protected function configure(): void
