992fcda9f59a8ce7bbb44eb35e86ec3d9e97cc29
H O S S E ! N <4.ho3ein@gmail.com>
2026-02-13 15:50:21 +0330

    feat: init command design

--- src/Commands/InitCommand.php
+++ src/Commands/InitCommand.php
@@ -2,6 +2,7 @@
 
 namespace Iwh3n\Tgram\Commands;
 
+use Iwh3n\Tgram\Config\ConfigManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,8 +20,16 @@ class InitCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        
-        $io->title('test');
+        $cm = new ConfigManager();
+
+        if ($cm->isConfigFile()) {
+            $io->writeln('<fg=red>[ERROR]</> The configuration file has already been created.');
+            return Command::FAILURE;
+        }
+
+        $path = $cm->createConfigFile();
+
+        $io->writeln("<fg=green>[OK]</> The configuration file in <options=underscore,bold>$path</> was successfully created.");
 
         return Command::SUCCESS;
     }
