e96ab270b2ab2f19717495de5c936f99ae29ca25
H O S S E ! N <4.ho3ein@gmail.com>
2026-02-16 12:34:48 +0330

    refactor: simplify ConsoleApplication and registerCommands
    Removed unnecessary Application property and constructor assignment.
    Updated getCommands namespace path to match Console namespace.
    Changed registerCommands to directly add commands to parent Application.

--- src/Console/ConsoleApplication.php
+++ src/Console/ConsoleApplication.php
@@ -8,25 +8,22 @@ use Symfony\Component\Console\Command\Command;
 
 class ConsoleApplication extends Application
 {
-    private Application $app;
     private Finder $finder;
 
     public function __construct()
     {
-        $this->app = new Application("TGram", "2.0.0");
+        parent::__construct("TGram", "2.0.0");
         $this->finder = new Finder();
     }
 
-    public function registerCommands(): Application
+    public function registerCommands(): void
     {
         $commands = $this->getCommands();
         foreach ($commands as $command) {
             if (class_exists($command) and is_subclass_of($command, Command::class)) {
-                $this->app->addCommand(new $command);
+                $this->addCommand(new $command);
             }
         }
-
-        return $this->app;
     }
 
     private function getCommands(): array
@@ -37,8 +34,8 @@ class ConsoleApplication extends Application
 
         foreach ($files as $file) {
             $relativePath = $file->getRelativePathname();
-            $className = "Iwh3n\\Tgram\\Commands\\" . str_replace(["/", ".php"], ["\\", ""], $relativePath);
-            array_push($commands, $className);
+            $className = "Iwh3n\\Tgram\\Console\\Commands\\" . str_replace(["/", ".php"], ["\\", ""], $relativePath);
+            $commands[] = $className;
         }
 
         return $commands;
